6d08d6050f7b8a04430f70ceb2e2352b177f3ac7
Kamilla Youver <95626487+archisha69@users.noreply.github.com>
2022-06-22 16:26:01 +0800

    Add files via upload

--- cogs/Main.py
+++ cogs/Main.py
@@ -6,7 +6,6 @@ import discord
 import datetime
 import time
 import math
-import string
 from datetime import datetime
 from discord.errors import InvalidArgument
 from discord.ext import commands
@@ -20,18 +19,24 @@ class MainCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['help'])
-    @commands.cooldown(1, 1, commands.BucketType.user)
-    async def embed(self, ctx, search=None): ...
+#    @commands.command(aliases=['help'])
+#    @commands.cooldown(1, 1, commands.BucketType.user)
+#    async def embed(self, ctx, search=None):
+#        e = discord.Embed(title='Commands', description='')
+#        await ctx.send(embed=e)
     
     # cmds
-    @commands.command()
+    @commands.command(aliases=['eta'])
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def roll(self, ctx): ...
+    async def etime(self, ctx, floor:int, damage:int):
+        etime = int((floor+3)*round(10/(math.log(damage+1, 10)+1), 0) + 1)
+        await ctx.reply(f'Your ETA for Catacombs floor {floor} with {damage} DMG is: {etime}.')
     
-    @commands.command(aliases=[''])
+    @commands.command(aliases=['ehp'])
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def q(self, ctx): ...
+    async def ehealth(self, ctx, hp:int, defense:int):
+        v1 = 1 + defense/100
+        await ctx.reply(f'Your Effective Health with {hp} HP and {defense} DEF is: {int(hp*v1)}')
         
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
