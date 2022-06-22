4cfbce55f9717f658a6f6ca97cdfe6b0efaf6124
Kamilla Youver <95626487+archisha69@users.noreply.github.com>
2022-06-22 18:10:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from discord.ext.commands import *
 from keep_alive import keep_alive
 # end of modules
 
-bot = commands.Bot(command_prefix=',', intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=',', case_insensitive=True, intents=discord.Intents.all())
 owner = ["thatOneArchUser#5518", "valid-user#0300"]
 oid = [706697300872921088, 705462972415213588]
 bot.remove_command('help')
