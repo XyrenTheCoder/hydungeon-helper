4bf3f23716b43a432c7c2ba73dbb3e3a3634205b
Kamilla Youver <95626487+archisha69@users.noreply.github.com>
2022-06-23 09:34:41 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 from threading import Thread
+import random
 
 app = Flask('')
 
 @app.route('/')
 def main():
-    return "totally not online"
+    #return render_template("root.html")
+    return "i use arch btw", 200
+
+def getrnd():
+    rnd = random.randint(1000,9999)
+    if rnd == 6969:
+        rnd = random.randint(1000, 9999)
+    return rnd
 
 def run():
-    app.run(host="0.0.0.0", port=6969)
+    try:
+        app.run(host="0.0.0.0", port=6969)
+    except OSError:
+        app.run(host="0.0.0.0", port=getrnd())
 
 def keep_alive():
-    s = Thread(target=run)
-    s.start()
+    server = Thread(target=run)
+    server.start()
